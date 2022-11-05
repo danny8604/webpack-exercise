4e73c89ba5b85a6e30fdbe8fe0ef9058bf85c8d3
u0454 <zx320320@gmail.com>
2022-11-05 23:27:37 +0800

    NOOOOOOOOOOOOOOOOOO

---
+++ src/tesla.js
@@ -0,0 +1,7 @@
+import Heading from "./components/heading/heading.js";
+import TeslaImage from "./components/Tesla-image/Tesla.image.js";
+
+const heading = new Heading();
+heading.render();
+const teslaImage = new TeslaImage();
+teslaImage.render();
